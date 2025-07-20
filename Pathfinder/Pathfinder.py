265506116c6e3a75b7f13dfd1bccadacd854184c
tBane1995 <bboguslawski1995@gmail.com>
2025-07-20 17:50:23 +0200

    now map is object

--- Pathfinder/Pathfinder.py
+++ Pathfinder/Pathfinder.py
@@ -8,23 +8,6 @@ import numpy as np
 import heapq
 import time
 
-WINDOW_COLOR = sdl2.ext.Color(48, 48, 48)
-WINDOW_SIZE = (600, 480)
-
-tile_size = 8
-
-map_wdt = int(WINDOW_SIZE[0]/tile_size)
-map_hgh = int(WINDOW_SIZE[1]/tile_size)
-mapa = np.empty((map_hgh, map_wdt), dtype=str)
-
-empty_char = ':'
-obstacle_char = '@'
-start_char = 'S'
-end_char = 'X'
-path_char = '+'
-
-##############################
-
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -42,143 +25,163 @@ class Point:
     def __repr__(self):
         return f"({self.x},{self.y})"
 
-StartPoint = None
-EndPoint = None
+class Map:
+	
+    map_wdt : int
+    map_hgh : int
+    tiles = []
 
-##############################
+    empty_char = ':'
+    obstacle_char = '@'
+    start_char = 'S'
+    end_char = 'X'
+    path_char = '+'
 
-def clear_map():
-    for y in range(map_hgh):
-        for x in range(map_wdt):
-            mapa[y][x] = empty_char
+    StartPoint = None
+    EndPoint = None
 
-def generate_start_point():
-    global StartPoint
-    x = np.random.randint(0, map_wdt)
-    y = np.random.randint(0, map_hgh)
-    mapa[y][x] = start_char
-    StartPoint = Point(x, y)
+    def __init__(self, width: int, height: int):
+        self.map_wdt = width
+        self.map_hgh = height
+        self.tiles = np.empty((self.map_hgh, self.map_wdt), dtype=str)
 
-def generate_end_point():
-    global EndPoint
-    while True:
-        x = np.random.randint(0, map_wdt)
-        y = np.random.randint(0, map_hgh)
-        if mapa[y][x] != start_char:
-            mapa[y][x] = end_char
-            EndPoint = Point(x, y)
-            break
+    def clear(self):
+        for y in range(0,self.map_hgh):
+            for x in range(0,self.map_wdt):
+                self.tiles[y][x] = self.empty_char
 
-def generate_obstacles():
-    min_count = int(map_wdt * map_hgh * 0.05)
-    max_count = int(map_wdt * map_hgh * 0.1)
-    count = np.random.randint(min_count, max_count)
-    while count > 0:
-        x = np.random.randint(0, map_wdt-1)
-        y = np.random.randint(0, map_hgh-1)
-        if mapa[y][x] not in (start_char, end_char, obstacle_char):
-            mapa[y][x] = obstacle_char
-            count -= 1
+    def generate_start_point(self):
+        x = np.random.randint(0, self.map_wdt)
+        y = np.random.randint(0, self.map_hgh)
+        self.tiles[y][x] = self.start_char
+        self.StartPoint = Point(x, y)
 
-##############################
+    def generate_end_point(self):
+        while True:
+            x = np.random.randint(0, self.map_wdt)
+            y = np.random.randint(0, self.map_hgh)
+            if self.tiles[y][x] != self.start_char:
+                self.tiles[y][x] = self.end_char
+                self.EndPoint = Point(x, y)
+                break
 
-def heuristic(a: Point, b: Point):
-    return abs(a.x - b.x) + abs(a.y - b.y)
+    def generate_obstacles(self):
+        min_count = int(self.map_wdt * self.map_hgh * 0.05)
+        max_count = int(self.map_wdt * self.map_hgh * 0.1)
+        count = np.random.randint(min_count, max_count)
 
-def getNeighbours(p: Point):
-    neighbours = []
-    for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-        nx, ny = p.x + dx, p.y + dy
-        if 0 <= nx < map_wdt and 0 <= ny < map_hgh:
-            if mapa[ny][nx] != obstacle_char:
-                neighbours.append(Point(nx, ny))
-    return neighbours
+        while count > 0:
+            x = np.random.randint(0, self.map_wdt-1)
+            y = np.random.randint(0, self.map_hgh-1)
+            if self.tiles[y][x] not in (self.start_char, self.end_char, self.obstacle_char):
+                self.tiles[y][x] = self.obstacle_char
+                count -= 1
 
-def aStar(start: Point, goal: Point):
-    openSet = []
-    heapq.heappush(openSet, (0, start))
-    cameFrom = {}
-    costSoFar = {}
-    cameFrom[start] = None
-    costSoFar[start] = 0
 
-    while openSet:
-        _, current = heapq.heappop(openSet)
+    def heuristic(self, a: Point, b: Point):
+        return abs(a.x - b.x) + abs(a.y - b.y)
 
-        if current == goal:
-            path = []
-            while current:
-                path.append(current)
-                current = cameFrom[current]
-            path.reverse()
-            return path
+    def getNeighbours(self, p: Point):
+        neighbours = []
+        for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+            nx, ny = p.x + dx, p.y + dy
+            if 0 <= nx < self.map_wdt and 0 <= ny < self.map_hgh:
+                if self.tiles[ny][nx] != self.obstacle_char:
+                    neighbours.append(Point(nx, ny))
+        return neighbours
 
-        for neighbor in getNeighbours(current):
-            newCost = costSoFar[current] + 1
-            if neighbor not in costSoFar or newCost < costSoFar[neighbor]:
-                costSoFar[neighbor] = newCost
-                priority = newCost + heuristic(neighbor, goal)
-                heapq.heappush(openSet, (priority, neighbor))
-                cameFrom[neighbor] = current
+    def aStar(self, start: Point, goal: Point):
+        openSet = []
+        heapq.heappush(openSet, (0, start))
+        cameFrom = {}
+        costSoFar = {}
+        cameFrom[start] = None
+        costSoFar[start] = 0
 
-    return []
+        while openSet:
+            _, current = heapq.heappop(openSet)
 
-##############################
+            if current == goal:
+                path = []
+                while current:
+                    path.append(current)
+                    current = cameFrom[current]
+                path.reverse()
+                return path
 
-def draw_map(renderer, path=set()):
-    for y in range(map_hgh):
-        for x in range(map_wdt):
-            p = Point(x, y)
-            if p in path and p!=StartPoint and p!=EndPoint:
-                renderer.color = sdl2.ext.Color(128,48,48)
-            else:
-                tile = mapa[y][x]
+            for neighbor in self.getNeighbours(current):
+                newCost = costSoFar[current] + 1
+                if neighbor not in costSoFar or newCost < costSoFar[neighbor]:
+                    costSoFar[neighbor] = newCost
+                    priority = newCost + self.heuristic(neighbor, goal)
+                    heapq.heappush(openSet, (priority, neighbor))
+                    cameFrom[neighbor] = current
+
+        return []
+
+    def draw(self, renderer, tile_size : int, path=set()):
+        for y in range(self.map_hgh):
+            for x in range(self.map_wdt):
+                p = Point(x, y)
+                if p in path and p!=self.StartPoint and p!=self.EndPoint:
+                    renderer.color = sdl2.ext.Color(128,48,48)
+                else:
+                    tile = self.tiles[y][x]
                 
-                if tile == empty_char:
-                    renderer.color = sdl2.ext.Color(64, 64, 64)
+                    if tile == self.empty_char:
+                        renderer.color = sdl2.ext.Color(64, 64, 64)
                     
-                if tile == obstacle_char:
-                    renderer.color = sdl2.ext.Color(32, 32, 32)
+                    if tile == self.obstacle_char:
+                        renderer.color = sdl2.ext.Color(32, 32, 32)
                     
-                if tile == start_char:
-                    renderer.color = sdl2.ext.Color(48, 128, 48)
+                    if tile == self.start_char:
+                        renderer.color = sdl2.ext.Color(48, 128, 48)
                     
-                if tile == end_char:
-                    renderer.color = sdl2.ext.Color(48, 48, 128)
-            rect = sdl2.SDL_Rect(tile_size*x, tile_size*y, tile_size, tile_size)
-            renderer.fill(rect)
+                    if tile == self.end_char:
+                        renderer.color = sdl2.ext.Color(48, 48, 128)
+                rect = sdl2.SDL_Rect(tile_size*x, tile_size*y, tile_size, tile_size)
+                renderer.fill(rect)
 
-##############################
+def main():
 
-sdl2.ext.init()
-window = sdl2.ext.Window("Pathfinder", WINDOW_SIZE)
-window.show()
+    WINDOW_COLOR = sdl2.ext.Color(48,48,48)
+    WINDOW_SIZE = (600,480)
 
-renderer = sdl2.ext.Renderer(window)
+    tile_size = 8
+    width = int(WINDOW_SIZE[0] / tile_size)
+    height = int(WINDOW_SIZE[1] / tile_size)
 
-while True:
+    sdl2.ext.init()
+    window = sdl2.ext.Window("Island Generator", WINDOW_SIZE)
+    window.show()
+    renderer = sdl2.ext.Renderer(window)
+
+    while True:
+ 
+		# handle events
+        events = sdl2.ext.get_events()
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                sdl2.ext.quit()
+                sys.exit(0)
     
-    # handle events
-    events = sdl2.ext.get_events()
-    for event in events:
-        if event.type == sdl2.SDL_QUIT:
-            sdl2.ext.quit()
-            sys.exit(0)
-    
-    # update
-    clear_map()
-    generate_start_point()
-    generate_end_point()
-    generate_obstacles()
-    path = aStar(StartPoint, EndPoint)
-    
-    # render
-    renderer.color = WINDOW_COLOR
-    renderer.clear()
-    draw_map(renderer, set(path))
-    renderer.present()
-    
-    time.sleep(1)
+		# update
+        mapa = Map(width, height)
+        mapa.clear()
+        mapa.generate_start_point()
+        mapa.generate_end_point()
+        mapa.generate_obstacles()
 
-sdl2.ext.quit()
-sys.exit(0)
+        path = mapa.aStar(mapa.StartPoint, mapa.EndPoint)
+
+        # render
+        renderer.color = WINDOW_COLOR
+        renderer.clear()
+        mapa.draw(renderer, tile_size, path)
+        renderer.present()
+		
+        del mapa
+        time.sleep(1)
+
+if __name__ == '__main__':
+	main()
